75056544fd43db13a1b36a36289a7090496741c9
WeiTsungCheng <forwindsun1359@gmail.com>
2024-03-17 17:21:41 +0800

    initial commit

---
+++ InterviewHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewHW
+//
+//  Created by WEI-TSUNG CHENG on 2024/3/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
