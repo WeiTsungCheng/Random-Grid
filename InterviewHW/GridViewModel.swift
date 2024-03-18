9fd75d19886a92de2f83f7b460b9416c2273f879
WeiTsungCheng <forwindsun1359@gmail.com>
2024-03-18 17:30:07 +0800

    [feat] set timer to create random coordinate

--- InterviewHW/GridViewModel.swift
+++ InterviewHW/GridViewModel.swift
@@ -9,6 +9,10 @@ import Foundation
 
 class GridViewModel {
     
+    typealias Observer<T> = (T) -> Void
+    var randomCoordinate: Observer<(x: Int, y: Int)>?
+    private var timer: Timer?
+    
     var m: Int
     var n: Int
     
@@ -16,5 +20,26 @@ class GridViewModel {
         self.m = m
         self.n = n
     }
+    
+    func startTimer(interval: Double, repeats: Bool) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: repeats) { [weak self] _ in
+                guard let self = self else { return }
+                self.generateRandomCoordinate()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func generateRandomCoordinate() {
+        let x = Int.random(in: 1...m)
+        let y = Int.random(in: 1...n)
+        randomCoordinate?((x, y))
+    }
+    
     
 }
